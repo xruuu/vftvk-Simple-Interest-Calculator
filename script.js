842c8519a70bfdefab695e3b9d8db9256e953242
xruuu <80094624+xruuu@users.noreply.github.com>
2021-03-05 17:31:10 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var d = new Date();
+    var y = d.getFullYear() + parseInt(document.getElementById("years").value);
+    var i = document.getElementById("amount").value *
+    document.getElementById("years").value *
+    document.getElementById("rate").value / 100 ;
+
+    if(i<0 || i==""){
+        alert("Please enter a valid number (Greater than 0). Thank you :3"); 
+        document.getElementById("amount").focus();
+    }
+    else {
+        document.getElementById("res").innerHTML=
+        "If you deposit <mark>" + document.getElementById("amount").value  + "</mark>,<br>" +
+        "at an interest rate of <mark>" + document.getElementById("rate").value + "%</mark>.<br>" +
+        "You will recieve an amount of <mark>" + Math.round(i) + "</mark>,<br>" + 
+        "in the year <mark>" + y + "</mark>";
+    }
+
+        
+
     
 }
-        
